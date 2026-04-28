7a20e705991535a6b5a7911094192243c98be729
JustRubik <danghuyhieu1434@gmail.com>
2026-04-28 18:23:11 +0700

    Writing sth dummy

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
+#include "main.h"
+
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main()
 {
-	cout << "Hello world" << endl;
+    cout << "--- STARTING SIMULATOR ---" << endl;
+    cout << "\n";
+    cout << "Please enter string to transmit: ";
+    
+    string s;
+    getline(cin, s);
+
+    cout << "Input string: " << s << endl;
+    cout << "\n--- RUNNING THE SIMULATOR ---\n" << endl;
+    /* 
+       Core() here 
+    */
+
+    cout << "Output string: "; // + output string right her
+
+    cout << "\n--- COMPARE TWO STRINGS ---" << endl;
+    // cout << result << endl;
+    
+    cout << "\nConclusion: " << " ---" << endl; // add conclusion here
+    cout << "\n--- FINISHING SIMULATOR ---" << endl;
+    // cout << "Hello world" << endl;
 	return 0;
 }
