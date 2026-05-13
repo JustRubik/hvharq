6b95dd2bcb7ef19cd9be2d05cc1acbee2ebe55bf
JustRubik <danghuyhieu1434@gmail.com>
2026-05-14 02:15:40 +0700

    thêm cmakepresets

--- include/core.h
+++ include/core.h
@@ -3,6 +3,7 @@
 #include <string>
 
 // Description: Core is a class which presenting the channel coding of the system, with input is a string that the transmitter transmits to the receiver through the channel and output is a string that the receiver receives, and they may be different from the other and vice versa.
+// Nói chung là, comment hơi dài
 
 class Core {
     private:
